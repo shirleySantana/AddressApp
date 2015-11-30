3c3d2f91ddbeab1e8b5743fd14a0386eac9fe094
ShirleyGirbelis <ShirleyGirbelis@192.168.1.38>
2015-11-30 20:51:26 +0100

    Finalizada

--- AddressApp/src/ch/makery/address/control/MainApp.java
+++ AddressApp/src/ch/makery/address/control/MainApp.java
@@ -10,6 +10,8 @@ import javax.xml.bind.Unmarshaller;
 
 import org.controlsfx.dialog.Dialogs;
 
+import com.sun.istack.internal.logging.Logger;
+
 import ch.makery.address.model.Person;
 import ch.makery.address.model.PersonListWrapper;
 import ch.makery.address.view.BirthdayStatisticsController;
@@ -19,11 +21,25 @@ import ch.makery.address.view.RootLayoutController;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -62,10 +78,59 @@ public class MainApp extends Application {
         // Set the application icon.
         this.primaryStage.getIcons().add(new Image("file:resources/images/Address_Book.png"));
 
-        initRootLayout();
+      
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(20);
+        grid.setVgap(20);
+        grid.setPadding(new Insets(50, 50, 50, 50));
 
-        showPersonOverview();
+        Text scenetitle = new Text("Welcome");
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(scenetitle, 0, 0, 2, 1);
+
+        Label userName = new Label("User Name:");
+        grid.add(userName, 0, 1);
+
+        TextField userTextField = new TextField();
+        grid.add(userTextField, 1, 1);
+
+        Label pw = new Label("Password:");
+        grid.add(pw, 0, 2);
+
+        PasswordField pwBox = new PasswordField();
+        grid.add(pwBox, 1, 2);
+
+        Button btn = new Button("Sign in");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(btn);
+        grid.add(hbBtn, 1, 4);
+
+        final Text actiontarget = new Text();
+        grid.add(actiontarget, 1, 6);
+
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent e) {
+                actiontarget.setFill(Color.FIREBRICK);
+                actiontarget.setText("Sign in button pressed");
+                
+                if (userTextField.getText().toString().equals("s") && pwBox.getText().toString().equals("s")) {
+                	  initRootLayout();
+
+                      showPersonOverview();
+                	
+                }
+            }
+        });
+
+        Scene scene = new Scene(grid, 450, 300);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
+ 
     public void initRootLayout() {
         try {
             // Load root layout from fxml file.
